d51db5f51cf353aa7cd17f344de0b59948c9d92d
jcinverno <81039195+jcinverno@users.noreply.github.com>
2022-04-03 18:22:05 +0000

    Add files via upload

---
+++ teste.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() 
+{
+    int favorite_number;
+    std::cout <<"Enter your favorite number betwen 1 and 100: ";
+    std::cin >> favorite_number;
+    std::cout << "Amazing!! That's my favorite number too! \n";
+    std::cout << "No really, " << favorite_number << " is my favorite number!";
+    return 0;
+}
