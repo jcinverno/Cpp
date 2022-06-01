c178abd9b4ae66f5eddbda9865644fed776f284b
jcinverno <81039195+jcinverno@users.noreply.github.com>
2022-06-01 10:47:54 +0100

    Update and rename Conversion_values_in_cents.cpp to Conversion_values_in_centss.cpp

--- Conversion_values_in_centss.cpp
+++ Conversion_values_in_centss.cpp
@@ -4,15 +4,8 @@ using namespace std;
 
 int main() {
 
-    int amount {0}; 
-    int dollars {0};
-    int quarters {0};
-    int dimes{0}, nickles{0}, pennies{0}, balance{0};
-    const int dollar {100};
-    const int quarter {25};
-    const int dime {10};
-    const int nickel {5};
-    const int penny {1};
+    int amount{0}, dollars{0}, quarters{0}, dimes{0}, nickles{0}, pennies{0}, balance{0};
+    const int dollar {100}, quarter {25}, dime {10}, nickel {5}, penny {1};
 
     cout << "Enter the amount in cents: "<<endl;
     cin >> amount;
